89f861adcafd2e88c4e4176736bdcc1333280e1d
Parag <paragdurafe@outlook.com>
2021-07-28 01:01:10 +0530

    sol 1.2

--- Array_and_Strings.py
+++ Array_and_Strings.py
@@ -1,6 +1,6 @@
 #Arrays And Strings
 #Sol 1.1
-user=input('Enter String : ')
+"""user=input('Enter String : ')
 lst_user=list(user)
 new=[]
 for val in lst_user:
@@ -8,4 +8,20 @@ for val in lst_user:
         new.append(val)
     else:
         pass
-print(new)
+print(new)"""
+
+#sol 1.2.1
+user=input('Enter String : ')
+lst_1=list(user)
+m=0
+n=len(user)-1
+lst_2=[]
+while m<=n:
+    lst_2.append(lst_1[n])
+    n-=1
+word=('').join(lst_2)
+print(word)
+
+#sol 1.2.2
+"""user=input('Enter String : ')
+print(user[::-1])"""
