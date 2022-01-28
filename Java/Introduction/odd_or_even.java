b9a5e853a56e3675a1d5388d136a21ee26348282
Parag <paragdurafe@outlook.com>
2022-01-28 23:55:01 +0530

    Introduction to Programming

--- Java/Introduction/odd_or_even.java
+++ Java/Introduction/odd_or_even.java
@@ -1,6 +1,5 @@
-package com.company;
+package com.first;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class odd_or_even {
--- Java/Introduction/pallindrome.java
+++ Java/Introduction/pallindrome.java
@@ -1,12 +1,25 @@
 package com.first;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class pallindrome {
     public static void main(String[] args) {
 
         Scanner st=new Scanner(System.in);
+        String empty="";
         String name=st.nextLine();
-        System.out.println(name);
+        int stringl=name.length();
+        for (int i=stringl-1;i>=0;--i){
+            empty=empty+name.charAt(i);
+        }
+
+        if (name.toLowerCase().equals(empty.toLowerCase())){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not Palindrome");
+        }
     }
+
 }
