8c54348ed4e42fe4bd36483b65af7065fb727fcd
Parag <paragdurafe@outlook.com>
2022-02-24 23:54:09 +0530

    Arrays

--- Java/Array/shuffle.java
+++ Java/Array/shuffle.java
@@ -1,20 +1,22 @@
 package com;
 
+import java.util.Arrays;
+
 public class shuffle {
     public static void main(String[] args) {
         int[] nums = {2,5,1,3,4,7};
         int n = 3;
+
+        System.out.println(Arrays.toString(shuffle(nums,n)));
     }
 
     static int[] shuffle(int[] arr,int n){
-        int j=0;
-        for (int i = 0; i < arr.length; i++) {
-            arr[i]=arr[j];
-            arr[i+1]=arr[j+ arr.length/2];
-            i++;
-            j++;
+        int[] newarr=new int[n*2];
+        for (int i = 0; i < n; i++) {
+            newarr[2*i]=arr[i];
+            newarr[2*i+1]=arr[i+n];
 
         }
-        return arr;
+        return newarr;
     }
 }
