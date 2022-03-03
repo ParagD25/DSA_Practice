0226e6d3a4aa9a57bc706a0f9af9edd4fb36037b
Parag <paragdurafe@outlook.com>
2022-03-03 23:53:52 +0530

    Arrays

--- Java/Array/transpose.java
+++ Java/Array/transpose.java
@@ -1,21 +1,24 @@
 package com;
 
-public class transpose {
+import java.util.Arrays;
+
+public class transpose{
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3},{4,5,6}};
-        trans(matrix);
+        System.out.println(Arrays.toString(trans(matrix)));
     }
 
-    static void trans(int[][] arr){
-        int[][] transarray=new int[arr[0].length][arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-
-                transarray[i][j]=arr[j][i];
-                System.out.print(transarray[i][j]+" ");
+    static int[][] trans(int[][] arr){
+        int w = arr[0].length;
+        int l = arr.length;
+        int[][] resultarr = new int[w][l];
+        for (int i = 0; i < w; i++){
+            for (int j = 0; j < l; j++){
+                resultarr[i][j] = arr[j][i];
             }
-            System.out.println();
         }
+        return resultarr;
 
     }
+
 }
