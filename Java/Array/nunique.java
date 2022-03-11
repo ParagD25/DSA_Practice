4e1b6b4a1ca68a682be1f0f063f023a8152bd4da
Parag <paragdurafe@outlook.com>
2022-03-11 23:57:23 +0530

    Arrays

--- Java/Array/nunique.java
+++ Java/Array/nunique.java
@@ -1,4 +1,18 @@
 package com;
 
+import java.util.Arrays;
+
 public class nunique {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(Arrays.toString(nuni(n)));
+    }
+
+    static int[] nuni(int n){
+        int[] res = new int[n];
+        for(int i=0; i<n; i++) {
+            res[i]  = (i*2)-n+1;
+        }
+        return res;
+    }
 }
