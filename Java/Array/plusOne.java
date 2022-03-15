4253405940c1c692e6214e5e34b13852504e5ef9
Parag <paragdurafe@outlook.com>
2022-03-15 23:57:42 +0530

    Arrays

--- Java/Array/plusOne.java
+++ Java/Array/plusOne.java
@@ -1,7 +1,33 @@
 package com;
 
+import java.util.Arrays;
+
 public class plusOne {
     public static void main(String[] args) {
+        int[] digits= {1,2,3};
 
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+    static int[] plusOne(int[] digits){
+
+        for (int i = digits.length - 1; i >=0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                break;
+            }
+
+            else {
+                digits[i] = 0;
+            }
+        }
+
+        if (digits[0] == 0) {
+            int[] res = new int[digits.length+1];
+            res[0] = 1;
+            return res;
+        }
+
+        return digits;
     }
 }
