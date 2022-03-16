86c1eef72e3258fcee4ca55bdc26b2f8605322fd
Parag <paragdurafe@outlook.com>
2022-03-16 23:44:41 +0530

    Arrays

--- Java/Array/maxsub.java
+++ Java/Array/maxsub.java
@@ -2,6 +2,27 @@ package com;
 
 public class maxsub {
     public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+
+    static int maxSubArray(int[] nums) {
+        int csum=nums[0];
+        int osum=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            if(csum>=0){
+                csum+=nums[i];
+            }
+            else{
+                csum=nums[i];
+            }
+
+            if (csum>osum){
+                osum=csum;
+            }
+        }
+        return osum;
 
     }
 }
--- Java/Array/reshape.java
+++ Java/Array/reshape.java
@@ -2,6 +2,40 @@ package com;
 
 public class reshape {
     public static void main(String[] args) {
+        int[][] mat = {{1,2},{3,4}};
+        int r = 1;
+        int c = 4;
+
 
     }
+
+    static  int[][] matrixReshape(int[][] mat, int r, int c) {
+        int row=mat.length;
+        int col=mat[0].length;
+
+        if((row*col)!=(r*c)){
+            return mat;
+        }
+
+        int[][] res=new int[r][c];
+        int rnum=0;
+        int cnum=0;
+
+        for (int i = 0; i <row ; i++) {
+            for (int j = 0; j <col ; j++) {
+                res[rnum][cnum]=mat[i][j];
+
+                cnum++;
+
+                if (cnum==c){
+                    cnum=0;
+                    rnum++;
+                }
+
+            }
+            
+        }
+        return res;
+    }
+
 }
