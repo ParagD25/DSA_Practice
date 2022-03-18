0bb02fcc704c0c454528a4d1059a1c7821747012
Parag <paragdurafe@outlook.com>
2022-03-18 23:59:21 +0530

    Arrays

--- Java/Array/minCost.java
+++ Java/Array/minCost.java
@@ -1,4 +1,23 @@
 package com;
 
 public class minCost {
+    public static void main(String[] args) {
+        int[] position = {2,2,2,3,3};
+        System.out.println(minCostToMoveChips(position));
+    }
+    
+    static int minCostToMoveChips(int[] position){
+        int even=0;
+        int odd=0;
+        for (int i = 0; i < position.length; i++) {
+
+            if (i%2==1){
+                odd++;
+            }
+            else even++;
+
+        }
+
+        return Math.min(odd,even);
+    }
 }
--- Java/Array/removeDupl.java
+++ Java/Array/removeDupl.java
@@ -2,6 +2,25 @@ package com;
 
 public class removeDupl {
     public static void main(String[] args) {
+        int[] nums = {1,1,2};
+        System.out.println(remove(nums));
+    }
 
+    static int remove(int[] num){
+
+        if (num.length==0){
+            return 0;
+        }
+
+        int i=0;
+        for (int j = 1; j < num.length; j++) {
+
+            if (num[i]!=num[j]){
+                i++;
+                num[i]=num[j];
+            }
+
+        }
+        return i+1;
     }
 }
