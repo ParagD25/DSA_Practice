1c8bc237ba6db6dfd534af58249c8c92c0b56fea
Parag <paragdurafe@outlook.com>
2022-03-19 23:59:25 +0530

    Arrays

--- Java/Array/minCost.java
+++ Java/Array/minCost.java
@@ -2,14 +2,14 @@ package com;
 
 public class minCost {
     public static void main(String[] args) {
-        int[] position = {2,2,2,3,3};
+        int[] position = {1,2,2,2,2};
         System.out.println(minCostToMoveChips(position));
     }
     
     static int minCostToMoveChips(int[] position){
         int even=0;
         int odd=0;
-        for (int i = 0; i < position.length; i++) {
+        for (int i :position) {
 
             if (i%2==1){
                 odd++;
